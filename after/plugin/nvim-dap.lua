4c7be62d3dd34d8e56f3aa38a69622c73d7223c2
Anthony Young <anthony.r.young@gmail.com>
2024-08-21 23:08:30 -0600

    Update dap configurations

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -1,4 +1,3 @@
-
 local dap = require("dap")
 local mason_registry = require("mason-registry")
 local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
@@ -13,17 +12,24 @@ dap.adapters.codelldb = {
     }
 }
 
+dap.adapters.lldb = {
+    name = "lldb",
+    type = "executable",
+    command = "/usr/bin/lldb",
+}
+
 dap.configurations.cpp = {
-    name = "Launch File",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-        return vim.fn.input("Path to executable", vim.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
+    {
+        name = "Launch File",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+    }
 }
 
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
-
