880ee44ea50adfa046f23051ec47c94e219f5356
Anthony Young <anthony.r.young@gmail.com>
2024-08-22 21:37:49 -0600

    Add debug configuration for rust

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -1,16 +1,4 @@
 local dap = require("dap")
-local mason_registry = require("mason-registry")
-local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
-local codelldb_path = codelldb_root .. "adaptor/codelldb"
-
-dap.adapters.codelldb = {
-    type = 'server',
-    port = "${port}",
-    executable = {
-        command = codelldb_path,
-        args = { "--port", "${port}" },
-    }
-}
 
 dap.adapters.lldb = {
     name = "lldb",
@@ -21,15 +9,82 @@ dap.adapters.lldb = {
 dap.configurations.cpp = {
     {
         name = "Launch File",
-        type = "codelldb",
+        type = "lldb",
         request = "launch",
         program = function()
             return vim.fn.input("Path to executable", vim.fn.getcwd() .. "/", "file")
         end,
+        args = {},
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        runInTerminal = false,
     }
 }
 
 dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+
+local ExecTypes = {
+  TEST = "cargo build --tests -q --message-format=json",
+  BIN = "cargo build -q --message-format=json"
+}
+
+local function runBuild(type)
+  local lines = vim.fn.systemlist(type)
+  local output = table.concat(lines, "\n")
+  local filename = output:match('^.*"executable":"(.*)",.*\n.*,"success":true}$')
+
+  if filename == nil then
+    return error("failed to build cargo project")
+  end
+
+  return filename
+end
+
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.exepath("codelldb"), -- install `lldb` && use :Mason to install codelldb & cpptools
+    args = { "--port", "${port}" },
+  },
+  name = "codelldb"
+}
+
+if vim.fn.has('win32') == 1 then
+	dap.adapters.codelldb.executable.detached = false
+end
+
+dap.configurations.rust = {
+  {
+    name = "Debug Test",
+    type = "codelldb",
+    request = "launch",
+    program = function ()
+      return runBuild(ExecTypes.TEST)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    showDisassembly = "never"
+  },
+  {
+    name = "Debug Bin",
+    type = "codelldb",
+    request = "launch",
+    program = function ()
+      return runBuild(ExecTypes.BIN)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    showDisassembly = "never"
+  }
+}
