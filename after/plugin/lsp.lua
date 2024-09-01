08d4094e7bed34c5c04a4f5510acaddaa47025af
Anthony Young <anthony.r.young@gmail.com>
2024-09-01 09:51:06 -0600

    Add descriptions for lsp usage

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,19 +3,17 @@ local lsp = require("lsp-zero")
 local lsp_attach = function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,{ buffer = bufnr, remap = false, desc = "[[LSP]] Goto definition" })
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = bufnr, remap = false, desc = "[[LSP]] Show Hover text" })
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, { buffer = bufnr, remap = false, desc = "[[LSP]] Search workspace symbols" })
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { buffer = bufnr, remap = false, desc = "[[LSP]] View diagnostic" })
+    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end, { buffer = bufnr, remap = false, desc = "[[LSP]] Goto next diagnostic" })
+    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { buffer = bufnr, remap = false, desc = "[[LSP]] Goto prev diagnostic" })
 
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-    vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { buffer = bufnr, remap = false, desc = "[[LSP]] View code action(s)" })
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { buffer = bufnr, remap = false, desc = "[[LSP]] View references" })
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { buffer = bufnr, remap = false, desc = "[[LSP]] Rename symbol" })
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = false, desc = "[[LSP]] Signature Help" })
 end
 
 lsp.extend_lspconfig({
