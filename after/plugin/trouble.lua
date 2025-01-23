268e56e0f282c1c242fbaa7b0bb52e76227ff0c7
Anthony Young <anthony.r.young@gmail.com>
2025-01-23 09:33:25 -0700

    Add trouble mapping to telescope for retaining results

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,4 +1,16 @@
 vim.keymap.set("n", "<leader>xq", "<cmd>Trouble diagnostics toggle<cr>",
-
-  {silent = true, noremap = true, desc = "Toggle Trouble"}
+    { silent = true, noremap = true, desc = "Toggle Trouble" }
 )
+
+local open_with_trouble = require('trouble.sources.telescope').open
+-- Use this to add more results to trouble without clearing the list
+-- local add_to_trouble = require('trouble.sources.telescope').add
+
+require('telescope').setup({
+    defaults = {
+        mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble },
+        }
+    }
+})
